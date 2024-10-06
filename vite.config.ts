4bb2db2aa4ff006e79f4e64941fcafee2a5a8e07
David Van Laatum <david@vanlaatum.id.au>
2024-10-07 00:03:13 +1030

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import commonjs from 'vite-plugin-commonjs';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
+  base: process.env.VITE_BASE_PATH ?? '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),

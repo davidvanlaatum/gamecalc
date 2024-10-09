8b68307c9aa3294217663b8240b95add675994bf
David Van Laatum <david@vanlaatum.id.au>
2024-10-09 19:05:10 +1030

    initial

--- src/data/MedievalDynasty/__tests__/items.test.ts
+++ src/data/MedievalDynasty/__tests__/items.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { getItemIcon, Item, ItemCategory, itemProperties } from '../index.ts';
 import ts from 'typescript';
 import { promises as fs } from 'fs';
@@ -42,56 +42,58 @@ describe('items', async () => {
 
   Object.entries(Item).forEach(([key, value]) => {
     if (itemProperties[value]?.synthetic !== true) {
-      test(`Item ${key} has properties`, () => {
-        expect(itemProperties[value], getLineNumber(key)).toBeDefined();
-      });
-      if (itemProperties[value] !== undefined) {
-        test(`Item ${key} has weight`, () => {
-          expect(itemProperties[value], getLineNumber(key)).toHaveProperty('weight', expect.any(Number));
+      describe(key, () => {
+        it(`has properties`, () => {
+          expect(itemProperties[value], getLineNumber(key)).toBeDefined();
         });
-        if (value !== Item.Coin) {
-          test(`Item ${key} has price`, () => {
-            expect(itemProperties[value], getLineNumber(key)).toHaveProperty('basePrice', expect.any(Number));
+        if (itemProperties[value] !== undefined) {
+          it(`has weight`, () => {
+            expect(itemProperties[value], getLineNumber(key)).toHaveProperty('weight', expect.any(Number));
           });
-        }
-        test(`Item ${key} has type`, () => {
-          const expectedType = itemProperties[value]?.provides?.some((provides) => provides.item == Item.Food)
-            ? ItemCategory.Food
-            : (expect.any(String) as string);
-          expect(itemProperties[value], getLineNumber(key)).toHaveProperty('type', expectedType);
-        });
-        if (itemProperties[value]?.type == ItemCategory.Food) {
-          test(`Item ${key} should provide food or water`, () => {
-            expect(
-              itemProperties[value]?.provides?.find((provides) => [Item.Food, Item.Water].includes(provides.item)),
-              getLineNumber(value),
-            ).not.toBeFalsy();
-          });
-        }
-        itemProperties[value]?.provides?.forEach((provides) => {
-          test(`Item ${key} provides ${provides.item}`, () => {
-            if (provides.amount > 0) {
-              expect(provides.priority, 'priority' + getLineNumber(key)).toBeGreaterThan(0);
-            }
-            expect(provides.amount, 'amount' + getLineNumber(key)).not.toEqual(0);
-          });
-          if (provides.item == Item.Food && value !== Item.Rot) {
-            test(`Item ${key} is food so should rot`, () => {
-              expect(itemProperties[value]).toHaveProperty('rots');
+          if (value !== Item.Coin) {
+            it(`has price`, () => {
+              expect(itemProperties[value], getLineNumber(key)).toHaveProperty('basePrice', expect.any(Number));
             });
           }
-        });
-      }
+          it(`has type`, () => {
+            const expectedType = itemProperties[value]?.provides?.some((provides) => provides.item == Item.Food)
+              ? ItemCategory.Food
+              : (expect.any(String) as string);
+            expect(itemProperties[value], getLineNumber(key)).toHaveProperty('type', expectedType);
+          });
+          if (itemProperties[value]?.type == ItemCategory.Food) {
+            it(`should provide food or water`, () => {
+              expect(
+                itemProperties[value]?.provides?.find((provides) => [Item.Food, Item.Water].includes(provides.item)),
+                getLineNumber(value),
+              ).not.toBeFalsy();
+            });
+          }
+          itemProperties[value]?.provides?.forEach((provides) => {
+            it(`provides ${provides.item}`, () => {
+              if (provides.amount > 0) {
+                expect(provides.priority, 'priority ' + getLineNumber(key)).toBeGreaterThan(0);
+              }
+              expect(provides.amount, 'amount ' + getLineNumber(key)).not.toEqual(0);
+            });
+            if (provides.item == Item.Food && value !== Item.Rot) {
+              it(`is food so should rot`, () => {
+                expect(itemProperties[value]).toHaveProperty('rots');
+              });
+            }
+          });
+        }
+      });
     }
   });
 });
 
 describe('getItemIcon', () => {
-  test('returns the correct icon for Log', () => {
+  it('returns the correct icon for Log', () => {
     expect(getItemIcon(Item.Log)).toEqual('/src/assets/MedievalDynasty/ItemsIcons/T_Icon_Log.png');
   });
   Object.entries(Item).forEach(([key, value]) => {
-    test(`returns the correct icon for ${key}`, () => {
+    it(`returns the correct icon for ${key}`, () => {
       expect(getItemIcon(value)).toContain('/src/assets/MedievalDynasty');
     });
   });
